42b96927e17aa239fe7daee5f109ba4083ba8459
Harsh Singh <harshsingh16d@gmail.com>
2025-05-13 23:15:03 +0530

    feat: added cohort1 feedback api

---
+++ src/app/api/cohort1feedback/route.ts
@@ -0,0 +1,19 @@
+// app/api/cohort2/route.ts
+import { NextResponse } from 'next/server';
+import { MongoClient } from 'mongodb';
+
+
+const uri = process.env.MONGO_URI!;
+const client = new MongoClient(uri);
+const dbName = 'cohortDB';
+const collectionName = 'cohort1feedback';
+
+export async function GET() {
+  try {
+    await client.connect();
+    const data = await client.db(dbName).collection(collectionName).find({}).toArray();
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+  }
+}
